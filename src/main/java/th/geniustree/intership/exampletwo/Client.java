416d95709cfc557fdd90525b83ba6240ced98da0
Umboon <uo_oom@hotmail.com>
2015-03-05 17:10:19 +0700

    Oom

---
+++ src/main/java/th/geniustree/intership/exampletwo/Client.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.geniustree.intership.exampletwo;
+
+import th.geniustree.intership.exampleone.Car;
+import th.geniustree.intership.exampleone.Xcar;
+
+/**
+ *
+ * @author oom
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Car xcar = new Xcar();
+        xcar.startEngin();
+        xcar.go();
+        System.out.println("Umboon");
+        
+    }
+
+}
